111570e3a52794fc1dff2b32fd13e66e46d8f829
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 12:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function johnLennonFacts(facts){
   }
   return array;
 }
+
+function iLoveTheBeatles(num){
+  var array = [];
+  do{
+    array.push("I love the Beatles!");
+    num++;
+  }
+  while(num<15);
+
+  reutrn array;
+}
